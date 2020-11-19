4cacedc21dc5fc1df70822524209e0366890e016
songjucho <pinetree134@ajou.ac.kr>
2020-11-19 17:38:30 +0900

    feat : ⚡️더미 TableView 구현
    - tableView cell selecting 구현

--- BoostPocket/BoostPocket/CountryListViewController.swift
+++ BoostPocket/BoostPocket/CountryListViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class CountryListViewController: UIViewController {
-
+    
+    var countries: [String] = ["a", "b", "c", "d", "e"]
+    
     @IBOutlet weak var countryListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        TableViewConfigure()
+        NavigationBarConfigure()
+    }
+    
+    private func TableViewConfigure() {
+        countryListTableView.register(UINib.init(nibName: CountryCell.identifier, bundle: .main), forCellReuseIdentifier: CountryCell.identifier)
+        countryListTableView.dataSource = self
+        countryListTableView.delegate = self
+    }
+    
+    private func NavigationBarConfigure() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTapped))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
         title = "여행할 나라를 선택해주세요"
@@ -24,6 +41,32 @@ class CountryListViewController: UIViewController {
     }
     
     @objc func doneButtonTapped() {
+        guard let selectedIndexPath = countryListTableView.indexPathForSelectedRow else { return }
+        print(countries[selectedIndexPath.row])
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CountryListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CountryCell.identifier, for: indexPath) as? CountryCell else { return UITableViewCell() }
+        cell.configure(with: countries[indexPath.row])
+        return cell
+    }
+}
+
+extension CountryListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .none
     }
 }
