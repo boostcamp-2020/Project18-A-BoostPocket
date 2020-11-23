a726f130fa3fb95570c28409a7efb9753ddfd5e7
sihyungyou <sihyungyou@gmail.com>
2020-11-23 15:40:05 +0900

    feat :⚡️ PersistenceManager 객체 생성
    - AppDelegate 에서 관리하던 persistenceContainer를 Manager 객체로 추상화

---
+++ BoostPocket/BoostPocket/PersistenceManager.swift
@@ -0,0 +1,52 @@
+//
+//  PersistenceManager.swift
+//  BoostPocket
+//
+//  Created by sihyung you on 2020/11/23.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+protocol PersistenceManagable: AnyObject {
+    var modelName: String { get }
+    var persistentContainer: NSPersistentContainer { get }
+    var context: NSManagedObjectContext { get }
+    func saveContext()
+}
+
+class PersistenceManager: PersistenceManagable {
+    private(set) var modelName = "datamodelTest"
+
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: modelName)
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                dump(error)
+            }
+        }
+        
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return self.persistentContainer.viewContext
+    }
+
+    // MARK: - Core Data Saving support
+
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                print(nserror.localizedDescription)
+            }
+        }
+    }
+}
