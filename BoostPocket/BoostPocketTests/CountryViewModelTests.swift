ba9131e6b18b1ae2b7a903b6840ba44b13746a04
sihyungyou <sihyungyou@gmail.com>
2020-11-25 00:13:54 +0900

    test :🕹 CountryListViewModel 테스트코드 기반 수정
    - fetch 시, removeAll 추가: removeAll하지 않을 경우, create 후 fetch할 경우 두번 들어가게됨
    - 테스트코드 정리
    - needFetchItem 함수 테스트 시 provider에 추가하고 테스트하도록 변경

--- BoostPocket/BoostPocketTests/CountryViewModelTests.swift
+++ BoostPocket/BoostPocketTests/CountryViewModelTests.swift
@@ -33,9 +33,10 @@ class CountryViewModelTests: XCTestCase {
         persistenceManager = nil
     }
     
-    func test_create_CountryItemViewModel() {
+    func test_countryItemViewModel_createInstance() {
         let country = CountryStub(name: countryName, flagImage: flag, currencyCode: currencyCode)
         let countryItemViewModel = CountryItemViewModel(country: country)
+        
         XCTAssertNotNil(countryItemViewModel)
         XCTAssertEqual(countryItemViewModel.name, countryName)
         XCTAssertEqual(countryItemViewModel.flag, flag)
@@ -44,33 +45,42 @@ class CountryViewModelTests: XCTestCase {
     
     func test_countryListViewModel_createCountry() {
         let country = countryListViewModel.createCountry(name: countryName, lastUpdated: lastUpdated, flagImage: flag, exchangeRate: exchangeRate, currencyCode: currencyCode)
+        
         XCTAssertNotNil(country)
         XCTAssertEqual(country, countryListViewModel.countries.first)
+        
         if let createdCountry = country {
             XCTAssertEqual(createdCountry.name, countryName)
-            XCTAssertEqual(createdCountry.name, countryName)
+            XCTAssertEqual(createdCountry.flag, flag)
+            XCTAssertEqual(createdCountry.currencyCode, currencyCode)
         }
     }
     
     func test_countryListViewModel_cellForItemAt() {
         countryListViewModel.createCountry(name: countryName, lastUpdated: lastUpdated, flagImage: flag, exchangeRate: exchangeRate, currencyCode: currencyCode)
-        let item = countryListViewModel.cellForItemAt(path: IndexPath(row: 0, section: 0))
-        XCTAssertEqual(item, countryListViewModel.countries.first)
+        
+        let countryItemViewModel = countryListViewModel.cellForItemAt(path: IndexPath(row: 0, section: 0))
+        
+        XCTAssertEqual(countryItemViewModel, countryListViewModel.countries.first)
+        XCTAssertEqual(countryItemViewModel.name, countryName)
+        XCTAssertEqual(countryItemViewModel.flag, flag)
+        XCTAssertEqual(countryItemViewModel.currencyCode, currencyCode)
     }
     
     func test_countryListViewModel_numberOfItem() {
-        countryListViewModel.createCountry(name: "test1", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 0.0, currencyCode: "11")
-        countryListViewModel.createCountry(name: "test2", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 12.0, currencyCode: "22")
-        countryListViewModel.createCountry(name: "test3", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 13.0, currencyCode: "33")
-        countryListViewModel.createCountry(name: "test4", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 14.0, currencyCode: "44")
-        countryListViewModel.createCountry(name: "test5", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 15.0, currencyCode: "55")
+        countryListViewModel.createCountry(name: "\(countryName)1", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 0.0, currencyCode: "\(currencyCode)1")
+        countryListViewModel.createCountry(name: "\(countryName)2", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 1.0, currencyCode: "\(currencyCode)2")
+        countryListViewModel.createCountry(name: "\(countryName)3", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 2.0, currencyCode: "\(currencyCode)3")
+        countryListViewModel.createCountry(name: "\(countryName)4", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 3.0, currencyCode: "\(currencyCode)4")
+        countryListViewModel.createCountry(name: "\(countryName)5", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 4.0, currencyCode: "\(currencyCode)5")
         
         XCTAssertEqual(countryListViewModel.numberOfItem(), 5)
     }
     
     func test_countryListViewModel_needFetchItem() {
-        countryProvider.createCountry(name: "test1", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 0.0, currencyCode: "11")
-        countryProvider.createCountry(name: "test2", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 12.0, currencyCode: "22")
+        countryProvider.createCountry(name: "\(countryName)12", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 0.0, currencyCode: "\(currencyCode)1")
+        countryProvider.createCountry(name: "\(countryName)23", lastUpdated: lastUpdated, flagImage: flag, exchangeRate: 0.0, currencyCode: "\(currencyCode)2")
+        
         countryListViewModel.needFetchItems()
         
         XCTAssertEqual(countryListViewModel.countries.count, 2)
