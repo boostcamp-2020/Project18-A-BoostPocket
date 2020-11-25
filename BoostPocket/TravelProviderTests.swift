840e725ffb83fe88d007a156a7921062ffc370b9
Jiniz <jiniz.ll@kakao.com>
2020-11-25 16:54:38 +0900

    test :🕹 travelProvider fetchTravels 테스트 코드 작성
    - travelProvider의 fetchTravels 테스트 코드 작성

--- BoostPocket/TravelProviderTests.swift
+++ BoostPocket/TravelProviderTests.swift
@@ -12,10 +12,18 @@ import XCTest
 class TravelProviderTests: XCTestCase {
     
     var persistenceManagerStub: PersistenceManagable!
+    var countryProviderStub: CountryProvidable!
     var travelProviderStub: TravelProvidable!
+    
+    let countryName = "대한민국"
+    let lastUpdated = Date()
+    let flagImage = Data()
+    let exchangeRate = 1.5
+    let currencyCode = "test code"
 
     override func setUpWithError() throws {
         persistenceManagerStub = PersistenceManagerStub()
+        countryProviderStub = CountryProvider(persistenceManager: persistenceManagerStub)
         travelProviderStub = TravelProvider(persistenceManager: persistenceManagerStub)
     }
 
@@ -24,16 +32,27 @@ class TravelProviderTests: XCTestCase {
         travelProviderStub = nil
     }
 
-    func test_travelProvier_createTravel() {
+    func test_travelProvider_createTravel() {
         
-        let countryName = "대한민국"
-        let countryProviderStub = CountryProvider(persistenceManager: persistenceManagerStub)
-        countryProviderStub.createCountry(name: countryName, lastUpdated: Date(), flagImage: Data(), exchangeRate: 1.0, currencyCode: "KRW")
+        countryProviderStub.createCountry(name: countryName, lastUpdated: lastUpdated, flagImage: flagImage, exchangeRate: exchangeRate, currencyCode: currencyCode)
         let createdTravel = travelProviderStub.createTravel(countryName: countryName)
         
         XCTAssertNotNil(createdTravel)
         XCTAssertEqual(createdTravel?.title, countryName)
         XCTAssertEqual(createdTravel?.country?.name, countryName)
+    }
+    
+    func test_travelProvider_fetchTravels() {
+        
+        XCTAssertEqual(travelProviderStub.fetchTravels(), [])
+        countryProviderStub.createCountry(name: countryName, lastUpdated: lastUpdated, flagImage: flagImage, exchangeRate: exchangeRate, currencyCode: currencyCode)
+        travelProviderStub.createTravel(countryName: countryName)
+        
+        XCTAssertNotEqual(travelProviderStub.fetchTravels(), [])
+        let travel = travelProviderStub.fetchTravels().first
+        
+        XCTAssertEqual(travel?.title, countryName)
+        XCTAssertEqual(travel?.exchangeRate, exchangeRate)
     }
 
 }
