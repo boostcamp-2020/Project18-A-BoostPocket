ee88409d946ca30b8005abc41552b3d9a8551715
songjucho <pinetree134@ajou.ac.kr>
2020-11-30 21:56:17 +0900

    feat : ⚡️ 여행 프로필 메모 수정 화면 추가
    - present까지 구현

--- BoostPocket/BoostPocket/TravelDetailScene/TravelProfileViewController.swift
+++ BoostPocket/BoostPocket/TravelDetailScene/TravelProfileViewController.swift
@@ -17,12 +17,24 @@ class TravelProfileViewController: UIViewController {
     var travelItemViewModel: TravelItemPresentable?
     weak var profileDelegate: TravelItemProfileDelegate?
     
+    @IBOutlet weak var travelMemoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(memoLabelTapped))
+        travelMemoLabel.addGestureRecognizer(tap)
     }
 
     @IBAction func deleteButtonTapped(_ sender: Any) {
         profileDelegate?.deleteTravel(id: travelItemViewModel?.id)
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func memoLabelTapped() {
+        let storyboard = UIStoryboard.init(name: "TravelDetail", bundle: nil)
+        let memoEditVC = storyboard.instantiateViewController(withIdentifier: "MemoEditViewController")
+        memoEditVC.modalPresentationStyle = .overFullScreen
+        memoEditVC.modalTransitionStyle = .crossDissolve
+        present(memoEditVC, animated: true, completion: nil)
     }
 }
