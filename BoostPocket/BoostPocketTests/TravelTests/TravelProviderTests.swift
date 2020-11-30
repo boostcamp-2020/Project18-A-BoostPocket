d26afe217cb04fe044dae27c502e9100422bcc4c
sihyungyou <sihyungyou@gmail.com>
2020-11-30 20:43:46 +0900

    test :🕹 TravelProvider Update Travel 기능 구현
    - TravelProvider Update Travel 함수 구현
    - Update Travel에 대한 테스트코드 작성

--- BoostPocket/BoostPocketTests/TravelTests/TravelProviderTests.swift
+++ BoostPocket/BoostPocketTests/TravelTests/TravelProviderTests.swift
@@ -14,28 +14,31 @@ class TravelProviderTests: XCTestCase {
     var persistenceManagerStub: PersistenceManagable!
     var countryProvider: CountryProvidable!
     var travelProvider: TravelProvidable!
+    var countryInfo: CountryInfo!
     var country: Country!
-    
+
     let countryName = "대한민국"
     let lastUpdated = Date()
     let flagImage = Data()
     let exchangeRate = 1.5
     let currencyCode = "test code"
-
+    
     override func setUpWithError() throws {
         persistenceManagerStub = PersistenceManagerStub()
         countryProvider = CountryProvider(persistenceManager: persistenceManagerStub)
         travelProvider = TravelProvider(persistenceManager: persistenceManagerStub)
         
         country = countryProvider.createCountry(name: countryName, lastUpdated: lastUpdated, flagImage: flagImage, exchangeRate: exchangeRate, currencyCode: currencyCode)
+        countryInfo = CountryInfo(name: countryName, lastUpdated: lastUpdated, flagImage: flagImage, exchangeRate: exchangeRate, currencyCode: currencyCode)
     }
-
+    
     override func tearDownWithError() throws {
         persistenceManagerStub = nil
         countryProvider = nil
         travelProvider = nil
+        countryInfo = nil
     }
-
+    
     func test_travelProvider_createTravel() {
         let createdTravel = travelProvider.createTravel(countryName: countryName)
         
@@ -55,6 +58,19 @@ class TravelProviderTests: XCTestCase {
         XCTAssertNotNil(fetchedTravel)
         XCTAssertEqual(fetchedTravel?.title, countryName)
         XCTAssertEqual(fetchedTravel?.exchangeRate, exchangeRate)
+    }
+    
+    func test_travelPrpvider_updateTravel() {
+        XCTAssertNotNil(persistenceManagerStub.createObject(newObjectInfo: countryInfo) as? Country)
+        
+        let createdTravel = travelProvider.createTravel(countryName: countryName)
+        XCTAssertNotNil(createdTravel)
+         
+        let travelInfo = TravelInfo(countryName: countryName, id: (createdTravel?.id)!, title: "updated title", memo: (createdTravel?.memo)!, startDate: (createdTravel?.startDate)!, endDate: (createdTravel?.endDate)!, coverImage: (createdTravel?.coverImage)!, budget: createdTravel!.budget, exchangeRate: createdTravel!.exchangeRate)
+        
+        XCTAssertNotNil(travelProvider.updateTravel(updatedTravelInfo: travelInfo))
+        XCTAssertEqual(travelProvider.fetchTravels().first, createdTravel)
+        XCTAssertEqual(travelProvider.fetchTravels().first?.title, "updated title")
     }
     
     func test_travelProvider_deleteTravel() {
