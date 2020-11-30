2f7311a20ef0b6e44f236e9818627256ad909895
Jiniz <jiniz.ll@kakao.com>
2020-11-30 23:24:47 +0900

    feat :⚡️ travel title modal 화면 생성
    - 여행 프로필의 title 수정을 위한 modal viewController 생성

--- BoostPocket/BoostPocket/TravelDetailScene/TitleEditViewController.swift
+++ BoostPocket/BoostPocket/TravelDetailScene/TitleEditViewController.swift
@@ -1,26 +1,29 @@
 //
-//  MemoEditViewController.swift
+//  TitleEditViewController.swift
 //  BoostPocket
 //
-//  Created by 송주 on 2020/11/30.
+//  Created by 이승진 on 2020/11/30.
 //  Copyright © 2020 BoostPocket. All rights reserved.
 //
 
 import UIKit
 
-class MemoEditViewController: UIViewController {
-    
+class TitleEditViewController: UIViewController {
+
     var saveButtonHandler: ((String) -> Void)?
     
-    @IBOutlet weak var memoTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
     
     @IBAction func saveButtonTapped(_ sender: UIButton) {
         dismiss(animated: true) { [weak self] in
-            self?.saveButtonHandler?(self?.memoTextView.text ?? "")
+            self?.saveButtonHandler?(self?.titleTextField.text ?? "")
         }
     }
+
 }
