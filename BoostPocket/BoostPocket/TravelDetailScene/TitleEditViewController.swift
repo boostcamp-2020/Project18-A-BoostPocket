a05bc75a6c5509a898ffdb5c07a7c6814b2077ab
songjucho <pinetree134@ajou.ac.kr>
2020-12-01 00:09:33 +0900

    feat : ⚡️ 여행 타이틀 입력 시 키보드가 뷰를 가리지 않도록 구현
    - 메모뷰와 마찬가지로 CGAffineTransform을 통해 뷰 움직임 적용

--- BoostPocket/BoostPocket/TravelDetailScene/TitleEditViewController.swift
+++ BoostPocket/BoostPocket/TravelDetailScene/TitleEditViewController.swift
@@ -12,6 +12,7 @@ class TitleEditViewController: UIViewController {
 
     var saveButtonHandler: ((String) -> Void)?
     
+    @IBOutlet weak var titleView: UIView!
     @IBOutlet weak var titleTextField: UITextField!
     
     override func viewDidLoad() {
@@ -20,10 +21,45 @@ class TitleEditViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        registerForKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unregisterForKeyboardNotifications()
+    }
+    
     @IBAction func saveButtonTapped(_ sender: UIButton) {
         dismiss(animated: true) { [weak self] in
             self?.saveButtonHandler?(self?.titleTextField.text ?? "")
         }
     }
+}
 
+extension TitleEditViewController {
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(note: NSNotification) {
+        if let keyboardSize = (note.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.titleView.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height / 3)
+            })
+        }
+    }
+    
+    @objc func keyboardWillHide(note: NSNotification) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.titleView.transform = .identity
+        })
+        
+    }
 }
