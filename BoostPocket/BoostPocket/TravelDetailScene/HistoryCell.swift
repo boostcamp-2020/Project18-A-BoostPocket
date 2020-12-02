af5e810c64240cc33f01f187d32fffc9e3f64bae
songjucho <pinetree134@ajou.ac.kr>
2020-12-02 21:46:59 +0900

    feat :⚡️ HistoryCell 구현
    - HistoryCell 화면 요소 구현

---
+++ BoostPocket/BoostPocket/TravelDetailScene/HistoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryCell.swift
+//  BoostPocket
+//
+//  Created by 송주 on 2020/12/02.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
