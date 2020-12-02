2fe578bdccf220e4ad9a409f63cb4129f224b013
songjucho <pinetree134@ajou.ac.kr>
2020-12-02 22:10:33 +0900

    feat :⚡️ HistoryListViewController 생성
    - HistoryListTableView 생성 및 IBOutlet 연결

---
+++ BoostPocket/BoostPocket/TravelDetailScene/HistoryListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryListViewController.swift
+//  BoostPocket
+//
+//  Created by 송주 on 2020/12/02.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import UIKit
+
+class HistoryListViewController: UIViewController {
+
+    @IBOutlet weak var HistoryListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
