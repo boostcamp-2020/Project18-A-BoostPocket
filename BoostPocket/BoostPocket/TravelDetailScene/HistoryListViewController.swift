ff78085c8d6e56b11fe41213b273c28a6993097f
songjucho <pinetree134@ajou.ac.kr>
2020-12-02 23:18:24 +0900

    feat : ⚡️ 지출 목록 테이블뷰 Header 모델 구현
    - 날짜, 금액 등이 담긴 header 데이터 구조체 구현

--- BoostPocket/BoostPocket/TravelDetailScene/HistoryListViewController.swift
+++ BoostPocket/BoostPocket/TravelDetailScene/HistoryListViewController.swift
@@ -17,11 +17,17 @@ struct DummyHistory: Hashable {
     var date: Date
 }
 
+struct HistoryListSectionHeader: Hashable {
+    var dayNumber: Int
+    var date: Date
+    var amount: Double
+}
+
 import UIKit
 
 class HistoryListViewController: UIViewController {
-    typealias DataSource = UITableViewDiffableDataSource<HistoryListSection, DummyHistory>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<HistoryListSection, DummyHistory>
+    typealias DataSource = UITableViewDiffableDataSource<HistoryListSectionHeader, DummyHistory>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<HistoryListSectionHeader, DummyHistory>
 
     @IBOutlet weak var historyListTableView: UITableView!
     
@@ -32,10 +38,10 @@ class HistoryListViewController: UIViewController {
         historyListTableView.delegate = self
         historyListTableView.register(HistoryCell.getNib(), forCellReuseIdentifier: HistoryCell.identifier)
         let dummyVM = [
+            DummyHistory(category: .income, title: "수입", amount: 100000, date: "2020-10-23".convertToDate()),
             DummyHistory(category: .food, title: "마라탕", amount: 18000, date: "2020-11-23".convertToDate()),
             DummyHistory(category: .food, title: "파스타", amount: 12000, date: "2020-11-25".convertToDate()),
-            DummyHistory(category: .food, title: "김치찌개", amount: 10000, date: "2020-12-01".convertToDate()),
-            DummyHistory(category: .income, title: "수입", amount: 100000, date: "2020-10-23".convertToDate())
+            DummyHistory(category: .food, title: "김치찌개", amount: 10000, date: "2020-12-01".convertToDate())
         ]
         applySnapshot(with: dummyVM)
     }
@@ -52,11 +58,32 @@ class HistoryListViewController: UIViewController {
     
     private func applySnapshot(with histories: [DummyHistory]) {
         var snapshot = Snapshot()
-        snapshot.appendSections([.main])
+        let sections = self.setupSection(with: histories)
+        snapshot.appendSections(sections)
         histories.forEach { history in
-            snapshot.appendItems([history], toSection: .main)
+            if let section = sections.filter({ Calendar.current.isDate(history.date, inSameDayAs: $0.date) }).first {
+                snapshot.appendItems([history], toSection: section)
+            }
         }
         dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    private func setupSection(with histories: [DummyHistory]) -> [HistoryListSectionHeader] {
+        var dayNumber = 1
+        var days = Set<HistoryListSectionHeader>()
+        histories.forEach { history in
+            let amount = history.amount
+            let date = history.date
+            if var day = days.filter({ Calendar.current.isDate(date, inSameDayAs: $0.date) }).first {
+                day.amount += amount
+            } else {
+                days.insert(HistoryListSectionHeader(dayNumber: dayNumber, date: date, amount: amount))
+                dayNumber += 1
+            }
+        }
+        var sections = [HistoryListSectionHeader](days)
+        sections = sections.sorted(by: {$0.date < $1.date})
+        return sections
     }
 }
 
