4205afab16628214a0f6569d07d0e7e74589325b
songjucho <pinetree134@ajou.ac.kr>
2020-12-03 16:28:06 +0900

    feat : ⚡️ Date 관련 extension 추가
    - 날짜 사이 기간들 구하는 extension 추가

--- BoostPocket/BoostPocket/Extensions/Date+ConvertToString.swift
+++ BoostPocket/BoostPocket/Extensions/Date+ConvertToString.swift
@@ -12,6 +12,8 @@ enum DateFormats: String {
     case dotted = "yyyy. MM. dd."
     case dashed = "yyyy-MM-dd."
     case korean = "yyyy년 MM월 dd일"
+    case month = "MM월"
+    case day = "dd"
     case time = "hh:mm"
 }
 
