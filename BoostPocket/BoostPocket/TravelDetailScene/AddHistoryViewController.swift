d31ff860c7b9cde6056ab47aa1459aacd0ddb49c
sihyungyou <sihyungyou@gmail.com>
2020-12-03 14:59:56 +0900

    feat :⚡️ Pull to Reqeust 기능 틀 잡기
    - History List VC에서 테이블뷰를 끌어당겼을 때 modal 띄우는 틀 구현

---
+++ BoostPocket/BoostPocket/TravelDetailScene/AddHistoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddHistoryViewController.swift
+//  BoostPocket
+//
+//  Created by sihyung you on 2020/12/03.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import UIKit
+
+class AddHistoryViewController: UIViewController {
+    static let identifier = "AddHistoryViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
--- BoostPocket/BoostPocket/TravelDetailScene/HistoryListViewController.swift
+++ BoostPocket/BoostPocket/TravelDetailScene/HistoryListViewController.swift
@@ -34,9 +34,16 @@ class HistoryListViewController: UIViewController {
     
     @IBOutlet weak var historyListTableView: UITableView!
 
+    weak var travelItemViewModel: HistoryListPresentable?
     private lazy var dataSource = configureDatasource()
     private lazy var headers = setupSection(with: travelItemViewModel?.histories ?? [])
-    weak var travelItemViewModel: HistoryListPresentable?
+    private lazy var refresher: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = .black
+        refreshControl.addTarget(self, action: #selector(addHistory), for: .valueChanged)
+        
+        return refreshControl
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,7 +60,15 @@ class HistoryListViewController: UIViewController {
         
     }
 
+    @objc private func addHistory() {
+        let addHistoryVC = AddHistoryViewController(nibName: AddHistoryViewController.identifier, bundle: nil)
+        self.present(addHistoryVC, animated: true) {
+            print("pull to refresh")
+        }
+    }
+    
     private func configureTableView() {
+        historyListTableView.refreshControl = refresher
         historyListTableView.delegate = self
         historyListTableView.register(HistoryCell.getNib(), forCellReuseIdentifier: HistoryCell.identifier)
         historyListTableView.register(HistoryHeaderCell.getNib(), forHeaderFooterViewReuseIdentifier: HistoryHeaderCell.identifier)
@@ -108,7 +123,11 @@ extension HistoryListViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: HistoryHeaderCell.identifier) as? HistoryHeaderCell else { return UIView() }
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: HistoryHeaderCell.identifier) as? HistoryHeaderCell,
+            // TODO: - 더 효율적으로 빈 headers 처리하는 방법 고민하기
+            !headers.isEmpty
+            else { return nil }
+        
         headerView.configure(with: headers[section].dayNumber, date: headers[section].date, amount: headers[section].amount)
         return headerView
     }
