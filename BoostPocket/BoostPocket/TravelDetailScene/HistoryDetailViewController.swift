41a7d23551711b9dd44ed44905a3b7312074bdfd
Jiniz <jiniz.ll@kakao.com>
2020-12-03 18:20:49 +0900

    feat :⚡️ History 상세 화면 구현
    - History 상세 화면 구현
    - 지출과 수입인 경우를 모두 포함

---
+++ BoostPocket/BoostPocket/TravelDetailScene/HistoryDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryDetailViewController.swift
+//  BoostPocket
+//
+//  Created by 이승진 on 2020/12/03.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import UIKit
+
+class HistoryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
