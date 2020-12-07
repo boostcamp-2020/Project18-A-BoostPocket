da13e5a80b6381037b1e88f5b765c2fdbc550149
songjucho <pinetree134@ajou.ac.kr>
2020-12-07 15:26:25 +0900

    test : 🕹 filterHistories 실패하는 테스트 작성
    - 메소드 구현 전, 실패하는 테스트 작성
    - 총 9가지 경우의 수에 대한 모든 테스트 작성

--- BoostPocket/BoostPocketTests/HistoryTests/HistoryFilterTests.swift
+++ BoostPocket/BoostPocketTests/HistoryTests/HistoryFilterTests.swift
@@ -16,15 +16,15 @@ class HistoryFilterTests: XCTestCase {
     
     override func setUpWithError() throws {
         historyFilter = HistoryFilter()
-        let history1 = HistoryStub(id: UUID(), isIncome: false, title: "All1", memo: "all1", amount: 11000, image: Data(), date: Date(), category: 0, isCard: nil, isPrepare: false)
-        let history2 = HistoryStub(id: UUID(), isIncome: false, title: "All2", memo: "cash1", amount: 12000, image: Data(), date: Date(), category: 0, isCard: false, isPrepare: false)
-        let history3 = HistoryStub(id: UUID(), isIncome: false, title: "All3", memo: "card1", amount: 13000, image: Data(), date: Date(), category: 0, isCard: true, isPrepare: false)
-        let history4 = HistoryStub(id: UUID(), isIncome: false, title: "Prepare1", memo: "all2", amount: 14000, image: Data(), date: Date(), category: 0, isCard: nil, isPrepare: true)
-        let history5 = HistoryStub(id: UUID(), isIncome: false, title: "Prepare2", memo: "cash2", amount: 15000, image: Data(), date: Date(), category: 0, isCard: false, isPrepare: true)
-        let history6 = HistoryStub(id: UUID(), isIncome: false, title: "Prepare3", memo: "card2", amount: 16000, image: Data(), date: Date(), category: 0, isCard: true, isPrepare: true)
-        let history7 = HistoryStub(id: UUID(), isIncome: false, title: "Date1", memo: "all3", amount: 17000, image: Data(), date: Date(), category: 0, isCard: nil, isPrepare: nil)
-        let history8 = HistoryStub(id: UUID(), isIncome: false, title: "Date2", memo: "cash3", amount: 18000, image: Data(), date: Date(), category: 0, isCard: false, isPrepare: nil)
-        let history9 = HistoryStub(id: UUID(), isIncome: false, title: "Date3", memo: "card3", amount: 19000, image: Data(), date: Date(), category: 0, isCard: true, isPrepare: nil)
+        let history1 = HistoryStub(id: UUID(), isIncome: false, title: "All1", memo: "All1", amount: 11000, image: Data(), date: Date(), category: 0, isCard: nil, isPrepare: false)
+        let history2 = HistoryStub(id: UUID(), isIncome: false, title: "All2", memo: "Cash1", amount: 12000, image: Data(), date: Date(), category: 0, isCard: false, isPrepare: false)
+        let history3 = HistoryStub(id: UUID(), isIncome: false, title: "All3", memo: "Card1", amount: 13000, image: Data(), date: Date(), category: 0, isCard: true, isPrepare: false)
+        let history4 = HistoryStub(id: UUID(), isIncome: false, title: "Prepare1", memo: "All2", amount: 14000, image: Data(), date: Date(), category: 0, isCard: nil, isPrepare: true)
+        let history5 = HistoryStub(id: UUID(), isIncome: false, title: "Prepare2", memo: "Cash2", amount: 15000, image: Data(), date: Date(), category: 0, isCard: false, isPrepare: true)
+        let history6 = HistoryStub(id: UUID(), isIncome: false, title: "Prepare3", memo: "Card2", amount: 16000, image: Data(), date: Date(), category: 0, isCard: true, isPrepare: true)
+        let history7 = HistoryStub(id: UUID(), isIncome: false, title: "Date1", memo: "All3", amount: 17000, image: Data(), date: Date(), category: 0, isCard: nil, isPrepare: nil)
+        let history8 = HistoryStub(id: UUID(), isIncome: false, title: "Date2", memo: "Cash3", amount: 18000, image: Data(), date: Date(), category: 0, isCard: false, isPrepare: nil)
+        let history9 = HistoryStub(id: UUID(), isIncome: false, title: "Date3", memo: "Card3", amount: 19000, image: Data(), date: Date(), category: 0, isCard: true, isPrepare: nil)
 
         histories.append(HistoryItemViewModelStub(history: history1))
         histories.append(HistoryItemViewModelStub(history: history2))
@@ -67,6 +67,93 @@ class HistoryFilterTests: XCTestCase {
         XCTAssertTrue(historyFilter.isCardOnly!)
     }
     
-    func test_filterHistories() {
+    func test_filterHistories_All_All() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = false
+        historyFilter.selectedDate = nil
+        historyFilter.isCardOnly = nil
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
+    }
+    
+    func test_filterHistories_All_Card() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = false
+        historyFilter.selectedDate = nil
+        historyFilter.isCardOnly = true
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
+    }
+    
+    func test_filterHistories_All_Cash() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = false
+        historyFilter.selectedDate = nil
+        historyFilter.isCardOnly = false
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
+    }
+    
+    func test_filterHistories_Prepared_All() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = true
+        historyFilter.selectedDate = nil
+        historyFilter.isCardOnly = nil
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
+    }
+    
+    func test_filterHistories_Prepared_Card() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = true
+        historyFilter.selectedDate = nil
+        historyFilter.isCardOnly = true
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
+    }
+    
+    func test_filterHistories_Prepared_Cash() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = true
+        historyFilter.selectedDate = nil
+        historyFilter.isCardOnly = false
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
+    }
+    
+    func test_filterHistories_Date_All() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = nil
+        historyFilter.selectedDate = Date()
+        historyFilter.isCardOnly = nil
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
+    }
+    
+    func test_filterHistories_Date_Card() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = nil
+        historyFilter.selectedDate = Date()
+        historyFilter.isCardOnly = true
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
+    }
+    
+    func test_filterHistories_Date_Cash() {
+        let castedHistories = histories as [HistoryItemViewModel]
+        historyFilter.isPrepareOnly = nil
+        historyFilter.selectedDate = Date()
+        historyFilter.isCardOnly = false
+        let filteredHistories = historyFilter.filterHistories(with: castedHistories)
+        XCTAssertNotNil(filteredHistories)
+        XCTAssertEqual(filteredHistories.count, 1)
     }
 }
--- BoostPocket/BoostPocketTests/HistoryTests/HistoryItemViewModelStub.swift
+++ BoostPocket/BoostPocketTests/HistoryTests/HistoryItemViewModelStub.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+@testable import BoostPocket
 
 class HistoryItemViewModelStub: HistoryItemViewModel {
     override init(history: HistoryProtocol) {
