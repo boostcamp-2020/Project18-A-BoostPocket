8c0133f0510039c662d6a1377f14722a97e6a697
Jiniz <jiniz.ll@kakao.com>
2020-12-07 15:54:18 +0900

    feat :⚡️ 화폐 단위 관련 extension 추가
    - 화폐 단위 관련 Extension 추가
    -> 정확하지 않은 locale 정보를 사용하므로 수정이 필요함
    - 수입 관련 데이터 일부 적용

---
+++ BoostPocket/BoostPocket/Extensions/Double+NumberFormat.swift
@@ -0,0 +1,9 @@
+//
+//  Double+NumberFormat.swift
+//  BoostPocket
+//
+//  Created by 이승진 on 2020/12/07.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import Foundation
---
+++ BoostPocket/BoostPocket/Extensions/String+CurrencySymbol.swift
@@ -0,0 +1,13 @@
+//
+//  Locale+CurrencySymbol.swift
+//  BoostPocket
+//
+//  Created by 이승진 on 2020/12/07.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import Foundation
+
+extension NSLocale {
+    
+}
