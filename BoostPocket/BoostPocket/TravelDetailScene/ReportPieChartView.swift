c64a337a99a55e8dd1934af2178be8bb342eca2b
songjucho <pinetree134@ajou.ac.kr>
2020-12-08 18:21:11 +0900

    feat : ⚡️ ReportVC, ReportPieChartView 생성
    - 파이그래프 그리기 위한 뷰 생성

---
+++ BoostPocket/BoostPocket/TravelDetailScene/ReportPieChartView.swift
@@ -0,0 +1,21 @@
+//
+//  ReportPieChartView.swift
+//  BoostPocket
+//
+//  Created by 송주 on 2020/12/08.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import UIKit
+
+class ReportPieChartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ BoostPocket/BoostPocket/TravelDetailScene/ReportViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReportViewController.swift
+//  BoostPocket
+//
+//  Created by 송주 on 2020/12/08.
+//  Copyright © 2020 BoostPocket. All rights reserved.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
